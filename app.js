0b75abc709bbf781f09bafd7c8b87e67ddc8933f
rudy <doomdexter07@gmail.com>
2022-08-29 18:02:41 +0530

    added global & async error handler

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
+const AppError = require("./utils/ErrorHandler");
 
 dotenv.config({ path: `${__dirname}/config` });
 
 //middlewares
 app.use(express.json());
+
+app.use(AppError);
 
 module.exports = app;
